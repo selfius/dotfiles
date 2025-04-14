52628c11c24b26bdcf7fc6aef56b527faaa707fb
Andrei Sviridov <self.polaris@gmail.com>
2025-04-13 21:23:23 -0400

    Box drawing plugin can now draw lines too

--- .config/nvim/lua/localplugins/boxes/plugin/boxes.lua
+++ .config/nvim/lua/localplugins/boxes/plugin/boxes.lua
@@ -1,14 +1,21 @@
-function draw_box(top_left, bottom_right)
-	region = vim.fn.getregion(top_left, bottom_right, {type ="\22", eol = true})
-	region_display_width = vim.fn.strdisplaywidth(region[1])
+local function next_utf8_char_offset(string, byte_offset_to_char)
+	char = vim.fn.strpart(string, 
+		byte_offset_to_char, 1, { chars = true })
+	print("the char is " .. char)
+	return byte_offset_to_char 
+		+ vim.fn.strlen(char)
+end
 
-	regionpos = vim.fn.getregionpos(top_left, bottom_right, {type ="\22", eol = true})
+local function draw_box(top_left, bottom_right)
+	local region = vim.fn.getregion(top_left, bottom_right, {type ="\22", eol = true})
+	local region_display_width = vim.fn.strdisplaywidth(region[1])
+
+	local regionpos = vim.fn.getregionpos(top_left, bottom_right, {type ="\22", eol = true})
 	for k, line_segment in next, regionpos do
-		line_num = line_segment[1][2];
-		line = vim.fn.getline(line_num)
+		local line_num = line_segment[1][2];
+		local line = vim.fn.getline(line_num)
 
-		--replacement = string.rep("━", region_display_width)
-		replacement = ''
+		local replacement = ''
 		if k == 1 then
 			replacement = '┏'
 			.. string.rep("━", region_display_width - 2)
@@ -24,27 +31,50 @@ function draw_box(top_left, bottom_right)
 			.. "┃"
 		end
 
-		-- line_segment hold position in bytes. 
-		-- Hence conversion to character idx, increment
-		-- and then conversion back to byte idx.
-		-- All because nvim_buf_set_text treats column ranges as end-exclusive.
-		line_end = vim.fn.byteidx(line, vim.fn.charidx(line, line_segment[2][3] - 1)+1)
-		line_end = line_end >= 0 and line_end or line_segment[2][3]+1
+		line_end = next_utf8_char_offset(line, line_segment[2][3] - 1) + 1
 
 		-- nvim_* api seem to treat positions as 0-based
 		-- while legacy functions - as 1-based
 		vim.api.nvim_buf_set_text(0, line_num - 1, line_segment[1][3] - 1,
-		line_num -1, line_end , {replacement})
+		line_num -1, line_end -1, {replacement})
 	end
 end
 
------- This is just gonna be here for the time being
+local function draw_line(top_left, bottom_right)
+	local region = vim.fn.getregion(top_left, bottom_right, {type ="\22", eol = true})
+	local region_display_width = vim.fn.strdisplaywidth(region[1])
+	local region_height = #region
+	if region_display_width == 1 then
+		-- draw a vertical line
+		local region_pos = vim.fn.getregionpos(top_left, bottom_right, {type ="\22", eol = true})
+		for k, line_pos in next, region_pos do
+			local column_pos = {
+				line = line_pos[1][2],
+				byte_offset = line_pos[1][3]
+			}
+			local line = vim.fn.getline(column_pos.line)
+			local next_char_pos = next_utf8_char_offset(line, 
+				column_pos.byte_offset -1) +1
+			vim.api.nvim_buf_set_text(0, column_pos.line - 1, 
+				column_pos.byte_offset - 1,
+				column_pos.line -1, next_char_pos - 1, {'│'})
+		end
+	else
+		-- draw a horizontal line
+		local line =  vim.fn.getline(top_left[2])
+		local line_end = next_utf8_char_offset(line, bottom_right[3] -1) +1
+		local replacement = string.rep("─", region_display_width);
+		vim.api.nvim_buf_set_text(0, top_left[2] - 1, top_left[3] - 1,
+		bottom_right[2] - 1, line_end - 1, {replacement})
+	end
+end
+
 vim.api.nvim_create_user_command('DrawBox', function (opts)
 	if vim.fn.mode() == "\22" then  -- visual block mode
 		-- fetch corners of the selection box
-		current_pos = vim.fn.getpos('.')
-		opposite_pos = vim.fn.getpos('v')
-		corners = {current_pos, opposite_pos}
+		local current_pos = vim.fn.getpos('.')
+		local opposite_pos = vim.fn.getpos('v')
+		local corners = {current_pos, opposite_pos}
 
 		table.sort(corners, function(a, b) 
 			if a[3] == b[3] then
@@ -54,8 +84,17 @@ vim.api.nvim_create_user_command('DrawBox', function (opts)
 			end
 		end)
 
-		-- change the lines with getline/setline
-		draw_box(corners[1], corners[2])
+
+		local region = vim.fn.getregion(corners[1], corners[2], {type ="\22", eol = true})
+		local region_display_width = vim.fn.strdisplaywidth(region[1])
+		local region_height = #region
+
+		-- TODO just pass regionpos (and possibly region to draw_line|box) functions
+		if region_height == 1 or region_display_width == 1 then
+			draw_line(corners[1], corners[2])
+		else
+			draw_box(corners[1], corners[2])
+		end
 	end
 end, 
 {nargs = 1, range = true})
